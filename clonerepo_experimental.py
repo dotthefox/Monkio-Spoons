99dbc8a98e0166c0a42446dce70383c0536a2c25
DeiantV <141984185+DeiantV@users.noreply.github.com>
2023-08-22 18:12:00 -0500

    Update clonerepo_experimental.py

--- clonerepo_experimental.py
+++ clonerepo_experimental.py
@@ -133,6 +133,8 @@ def run_script():
         run_cmd(f"git checkout 293c5f2c9e9bf08d910090076c692e79b513a0e3")
         run_cmd(f"git checkout 4b281493ddc4add0fac16b15d2a4619c8d8253b4")
         run_cmd(f"git checkout 25732bf7802618bbc82d96606ad19c04837848fc")
+        run_cmd(f"git checkout d4d636aa7183df945bb65ef22a2ba33f1a7ee598")
+        run_cmd(f"git checkout 7f86f614352bd2534c562348b932573a882d82b5")
 
 
         
