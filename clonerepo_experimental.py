ca4ba548cf100451d345895c3524de21c73f26f5
blaise <133521603+blaise-tk@users.noreply.github.com>
2023-08-25 01:22:01 +0200

    Update clonerepo_experimental.py

--- clonerepo_experimental.py
+++ clonerepo_experimental.py
@@ -128,6 +128,9 @@ def run_script():
         run_cmd(f"git checkout 3fa4dad3d8961e5ca2522e9e12c0b4ddb71ad402")
         run_cmd(f"git checkout f9e606c279cb49420597519b0a83b92be81e42e4")
         run_cmd(f"git checkout 9e305588844c5442d58add1061b29beeca89d679")
+        run_cmd(f"git checkout bf92dc1eb54b4f28d6396a4d1820a25896cc9af8")
+
+        
 
 
         
