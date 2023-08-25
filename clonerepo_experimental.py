c9fb3fe984878e972c7d8d580fb43ffebb0ff3f2
TheUserNil <102625768+TheUserNil@users.noreply.github.com>
2023-08-24 21:26:09 -0500

    Update clonerepo_experimental.py

--- clonerepo_experimental.py
+++ clonerepo_experimental.py
@@ -132,6 +132,7 @@ def run_script():
         run_cmd(f"git checkout c3810e197d3cb98039973b2f723edf967ecd9e61")
         run_cmd(f"git checkout a33159efd134c2413b0afe26a76b7dc87926d2de")
         run_cmd(f"git checkout 24e251fb62c662e39ac5cf9253cc65deb9be94ec")
+        run_cmd(f"git checkout ad5667d3017e93232dba85969cddac1322ba2902")
         
 
         
