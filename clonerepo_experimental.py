1d1b61f3123f671edd2322ee7193933c7183166e
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-08-31 14:59:04 +0200

    Update clonerepo_experimental.py

--- clonerepo_experimental.py
+++ clonerepo_experimental.py
@@ -143,6 +143,8 @@ def run_script():
         run_cmd(f"git checkout 7e02fae1ebf24cb151bf6cbe787d06734aa65862")
         run_cmd(f"git checkout 6aea5ea18ed0b9a1e03fa5d268d6bc3c616672a9")
         run_cmd(f"git checkout f0f9b25717e59116473fb42bd7f9252cfc32b398")
+        run_cmd(f"git checkout b394de424088a81fc081224bc27338a8651ad3b2")
+        
         # Edit the file here, before copying
         #edit_file(f"{temp_repo_path}/infer-web.py")
 
