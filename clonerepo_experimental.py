4ef44154c07067ce90a098c443df4a5a2c01f7b1
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-09-08 20:37:02 +0200

    Update clonerepo_experimental.py

--- clonerepo_experimental.py
+++ clonerepo_experimental.py
@@ -222,7 +222,7 @@ def download_pretrained_models():
         ]
     }
 
-    base_url = "https://huggingface.co/IAHispano/applio/"
+    base_url = "https://huggingface.co/IAHispano/applio/resolve/main"
     base_path = "/content/Applio-RVC-Fork/"
 
     # Calculate total number of files to download
